891d09805405fdb7528325676e26de0cb1b7b99a
unknown <am185219@WPHAM185219-1XS.corp.ncr.com>
2014-12-05 15:57:04 +0800

    init commit

---
+++ fuiture_test.go
@@ -0,0 +1,77 @@
+package fp
+
+import (
+	"fmt"
+	. "gopkg.in/check.v1"
+	"testing"
+	"time"
+)
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (suite *MySuite) TestPromise(c *C) {
+
+	didCall := false
+	p := Promise(func() (ret AnyVal, skip bool) {
+		fmt.Print("adrian guwapo")
+		didCall = true
+		return
+	})
+
+	wasTested := false
+	for i := 0; i < 50; i++ {
+		fmt.Print("*")
+		if 20 == i {
+			time.Sleep(300 * time.Millisecond)
+		} else if 21 == i {
+			c.Assert(didCall, Equals, true)
+			wasTested = true
+		}
+	}
+	c.Assert(wasTested, Equals, true)
+	c.Log("adrian guwapo 2")
+	<-p
+}
+
+func (suite *MySuite) TestRangeList(c *C) {
+	list := []int{1, 2, 3, 4, 5}
+
+	p := RangeList(func(a, i AnyVal) (ret AnyVal, skip bool) {
+		c.Log("i=", i)
+		ret = a.(int) * 3
+		return
+	}, list)
+
+	for i := range p {
+		c.Log(i.(int))
+	}
+}
+
+func (suite *MySuite) TestRangeDict(c *C) {
+	list := map[string]int{
+		"david": 4,
+		"mama":  22,
+		"papa":  31,
+	}
+
+	p := RangeDict(func(v, k AnyVal) (ret AnyVal, skip bool) {
+		c.Logf("v=%v, k=%v", v, k)
+		ret = v
+		if v.(int) != 4 {
+			skip = true
+		}
+		return
+	}, list)
+
+	hasItem := false
+	for i := range p {
+		hasItem = true
+		c.Assert(i.(int) == 4, Equals, true)
+	}
+
+	c.Assert(hasItem, Equals, true)
+}
---
+++ future.go
@@ -0,0 +1,153 @@
+package fp
+
+import (
+	"reflect"
+	"sync"
+)
+
+type AnyVal interface{}
+type PromiseChan chan AnyVal
+type LazyChan chan AnyVal
+type Func0 func() (AnyVal, bool)
+type Func1 func(a AnyVal) (AnyVal, bool)
+type Func2 func(a, b AnyVal) (AnyVal, bool)
+type Ranger func(Func2, AnyVal, ...int) PromiseChan
+
+func makepromise(chanlen ...int) (p PromiseChan) {
+	if 0 < len(chanlen) {
+		p = make(PromiseChan, chanlen[0])
+	} else {
+		p = make(PromiseChan)
+	}
+	return
+}
+
+func Promise(f Func0, chanlen ...int) (p PromiseChan) {
+
+	p = makepromise(chanlen...)
+	go func() {
+		defer close(p)
+		if ret, skip := f(); !skip {
+			p <- ret
+		}
+	}()
+	return p
+}
+
+func RangeList(f Func2, list AnyVal, chanlen ...int) (p PromiseChan) {
+	v, ok := list.(reflect.Value)
+	if !ok {
+		v = reflect.ValueOf(list)
+	}
+
+	n := v.Len()
+	p = makepromise(chanlen...)
+
+	wg := new(sync.WaitGroup)
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func(index int, ch PromiseChan) {
+			defer wg.Done()
+			if ret, skip := f(v.Index(index).Interface(), index); !skip {
+				ch <- ret
+			}
+
+		}(i, p)
+	}
+
+	go func() { wg.Wait(); close(p) }()
+
+	return p
+}
+
+// calls Func2 as func(value, key)
+func RangeDict(f Func2, dict AnyVal, chanlen ...int) (p PromiseChan) {
+	v, ok := dict.(reflect.Value)
+	if !ok {
+		v = reflect.ValueOf(dict)
+	}
+
+	n := v.Len()
+	p = makepromise(chanlen...)
+	wg := new(sync.WaitGroup)
+	wg.Add(n)
+	for _, vk := range v.MapKeys() {
+		go func(vk reflect.Value, ch PromiseChan) {
+			defer wg.Done()
+			if ret, skip := f(v.MapIndex(vk).Interface(), vk.Interface()); !skip {
+				ch <- ret
+			}
+
+		}(vk, p)
+	}
+
+	go func() { wg.Wait(); close(p) }()
+
+	return p
+}
+
+func Range(f Func2, listOrMap AnyVal, chanlen ...int) (p PromiseChan) {
+	typ := reflect.TypeOf(listOrMap)
+	var ranger Ranger
+	switch typ.Kind() {
+	case reflect.Slice:
+		ranger = RangeList
+	case reflect.Map:
+		ranger = RangeDict
+	default:
+		return
+	}
+
+	p = ranger(f, typ, chanlen...)
+	return
+}
+
+// !!! not yet tested
+// caller closes l and p
+func Lazy(f Func1) (p PromiseChan, l LazyChan) {
+	p = make(PromiseChan)
+	l = make(LazyChan)
+	go func() {
+		for input := range l {
+			ret, _ := f(input)
+			p <- ret
+		}
+	}()
+
+	return
+}
+
+// !!! not yet tested
+func Chain(ch LazyChan, a ...Func1) (p PromiseChan) {
+	if 0 >= len(a) {
+		return
+	}
+	p = make(PromiseChan)
+	go func() {
+		v, _ := a[0](<-ch)
+		for i := 1; i < len(a); i++ {
+			v, _ = a[i](v)
+		}
+		p <- v
+		close(p)
+	}()
+	return p
+}
+
+// !!! not yet tested
+func ParallelLoop(af Func2, bf Func1, aListOrMap AnyVal, chanlen ...int) (p PromiseChan) {
+
+	p = makepromise(chanlen...)
+	q1 := Range(af, aListOrMap, chanlen...)
+	go func() {
+		for qres := range q1 {
+			if ret, skip := bf(qres); !skip {
+				p <- ret
+			}
+		}
+		close(p)
+	}()
+
+	return
+
+}
