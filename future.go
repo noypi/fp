a28a85e0aed11e0e71a27ac97cfe2563da8b50d3
unknown <am185219@WPHAM185219-1XS.corp.ncr.com>
2016-03-09 15:08:05 +0800

    add futures

--- future.go
+++ future.go
@@ -17,6 +17,28 @@ func Future(f Func0) (p *Promise) {
 	return
 }
 
+func Future1(f Func1, param AnyVal) (p *Promise) {
+	p = makepromise()
+	go func() {
+		if ret, skip := f(param); !skip {
+			p.send(ret)
+		}
+		p.close()
+	}()
+	return
+}
+
+func FutureN(f FuncN, params ...AnyVal) (p *Promise) {
+	p = makepromise()
+	go func() {
+		if ret, skip := f(params...); !skip {
+			p.send(ret)
+		}
+		p.close()
+	}()
+	return
+}
+
 func fnFutureQ(f Func0, pin *Promise) (p *Promise) {
 	p = pin
 	go func() {
