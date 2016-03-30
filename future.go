17e72f549af79ba4e84b37edf328aa9b1e546ac7
davidking <adrian.migraso@gmail.com>
2016-03-30 21:53:27 +0800

    add FutureWithNotify

--- future.go
+++ future.go
@@ -13,6 +13,18 @@ func Future(f Func0) (p *Promise) {
 	return
 }
 
+func FutureWithNotify(fn func(chan AnyVal) (AnyVal, error)) (q *Promise, notify *Promise) {
+	ch := make(chan AnyVal, 1)
+	notify = PipeChan(ch)
+	q = Future(func() (ret AnyVal, err error) {
+		ret, err = fn(ch)
+		close(ch)
+		return
+	})
+
+	return
+}
+
 func Future1(f Func1, param AnyVal) (p *Promise) {
 	p = makepromise()
 	go func() {
