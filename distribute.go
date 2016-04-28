7e4b2f5bc3d5133c1eb7bd617f069dd5f94491be
Gogs <gogs@fake.local>
2016-04-28 21:19:49 +0800

    add distribute work

---
+++ distribute.go
@@ -0,0 +1,21 @@
+package fp
+
+import (
+	"sync"
+)
+
+func DistributeWork(src *Promise, worker func(AnyVal), nProcessor int) (q *Promise) {
+	q = makepromise()
+	go func() {
+		var wg sync.WaitGroup
+		var i int
+		for a := range q.q {
+			go func() { worker(a); wg.Done() }()
+			i++
+			if 0 == (nProcessor % i) {
+				wg.Wait()
+			}
+		}
+	}()
+	return
+}
