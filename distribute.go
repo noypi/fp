afa0486d41f83afe334376c27b1f634075c35e14
Gogs <gogs@fake.local>
2016-04-28 21:27:27 +0800

    -->todo, what to do with returned error

--- distribute.go
+++ distribute.go
@@ -4,13 +4,18 @@ import (
 	"sync"
 )
 
-func DistributeWork(src *Promise, worker func(AnyVal), nProcessor uint) (q *Promise) {
+func DistributeWork(src *Promise, worker func(AnyVal) (AnyVal, error), nProcessor uint) (q *Promise) {
 	q = makepromise()
 	go func() {
 		var wg sync.WaitGroup
 		var i uint
 		for a := range q.q {
-			go func() { worker(a); wg.Done() }()
+			go func() {
+				var msg = new(qMsg)
+				msg.a, msg.err = worker(a)
+				q.q <- msg
+				wg.Done()
+			}()
 			i++
 			if 0 >= (nProcessor - i) {
 				wg.Wait()
