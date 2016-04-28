3ee1d003dd142f9df70a44954c139637263fc39d
Gogs <gogs@fake.local>
2016-04-28 21:22:27 +0800

    use uint

--- distribute.go
+++ distribute.go
@@ -4,11 +4,11 @@ import (
 	"sync"
 )
 
-func DistributeWork(src *Promise, worker func(AnyVal), nProcessor int) (q *Promise) {
+func DistributeWork(src *Promise, worker func(AnyVal), nProcessor uint) (q *Promise) {
 	q = makepromise()
 	go func() {
 		var wg sync.WaitGroup
-		var i int
+		var i uint
 		for a := range q.q {
 			go func() { worker(a); wg.Done() }()
 			i++
